9c3c99cf599ef69b8086dfd38922668885a14449
Niklas Söderberg <nma.prob@gmail.com>
2021-03-16 15:45:30 +0100

    initialize project

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('Hello world!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
