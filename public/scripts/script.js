3961dcfb89c8c62b56fdf3fa9e054d32fbb4932a
Niklas Söderberg <nma.prob@gmail.com>
2021-03-22 14:59:55 +0100

    header, footer, sidebar

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,11 @@
-const btn = document.getElementById('submit');
-let by = document.getElementById('by');
-let content = document.getElementById('content');
-const form = document.getElementById('form');
-form.addEventListener('keyup', (e) => {
-    by.value === '' || content.value === ''
-        ? (btn.disabled = true)
-        : (btn.disabled = false);
-});
+// putting required on form was way better
+
+// const btn = document.getElementById('submit');
+// let by = document.getElementById('by');
+// let content = document.getElementById('content');
+// const form = document.getElementById('form');
+// form.addEventListener('keyup', (e) => {
+//     by.value === '' || content.value === ''
+//         ? (btn.disabled = true)
+//         : (btn.disabled = false);
+// });
